368c72f74ca3ac649dbda8bd8fc41bb8e0e69c80
ganesh nb <ganeshbhatkalkar2005@gmail.comgit config --list>
2025-09-30 11:35:12 +0530

    basic changes

---
+++ portfolio/src/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {  
+    return (
+        <nav className="navbar">
+            <h1 className="myname">Ganesh NB</h1>                                  
+        </nav>
+    )
+}
+
+export default Navbar
