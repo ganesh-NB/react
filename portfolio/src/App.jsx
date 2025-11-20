c1b515f30657669facb61ff0493a5a4d8b27e6a0
ganesh nb <ganeshbhatkalkar2005@gmail.comgit config --list>
2025-11-20 21:25:51 +0530

    new change

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,14 +3,22 @@ import Home from './home.jsx';
 import Navbar from './navbar.jsx' ;
 import Skills from './skills.jsx';
 
-
-function App() {
+const Message = (props) => {
+  const {text,color}=props;
+  console.log(props);
+  return <div style={{backgroundColor:color}}>
+    {text}
+  </div>
+}
+const App = () => {
   return (
     <>
-  
-    <Navbar/>
-    <Home/>
-    <Skills/>
+      <Message text="how are you" color="orange" />
+      <Message text="I am fine" color="white" />
+      <Message text="Ok bye then" color="green" />
+
+
+      <h1></h1>
     </>
   )
 }
